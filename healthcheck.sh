bbe201c679bc42a4327f426a9c440a514df690a9
sandypk100 <48682903+sandypk100@users.noreply.github.com>
2021-09-22 23:39:22 +0530

    Create healthcheck.sh

---
+++ healthcheck.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Environment Variables
+# HUB_HOST
+# BROWSER
+# MODULE
+
+echo "Checking if hub is ready - $HUB_HOST"
+
+while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
+do
+        sleep 3
+done
+echo "hub is ready"
+
+# start the java command
+pytest
